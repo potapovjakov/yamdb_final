fe175c88cea93d958662de8db51ea311c4f9d439
Yakov <93822070+potapovjakov@users.noreply.github.com>
2022-07-01 23:53:44 +0300

    delete admin

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -1,4 +1,3 @@
-from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
 
@@ -18,6 +17,5 @@ urlpatterns = [
         TemplateView.as_view(template_name='api/redoc.html'),
         name='redoc'
     ),
-    #path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
